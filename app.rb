13921cb909234c7e9f657876d54de4576b0b34d8
Michel Martens & Damian Janowski <michel+djanowski@soveran.com>
2010-09-22 23:59:31 -0300

    Simple layout.

--- app.rb
+++ app.rb
@@ -4,19 +4,25 @@ require "rdiscount"
 require "json"
 
 Cuba.define do
+  def haml(template, locals = {})
+    render("views/layout.haml", content: render("views/#{template}.haml", locals))
+  end
+
   on get, path("commands") do
     on segment do |name|
       @name = name.upcase
       @command = JSON.parse(File.read("redis-doc/commands.json"))[@name]
       @body = render("redis-doc/commands/#{name}.md")
+      @title = @name
 
-      res.write render("views/commands/name.haml")
+      res.write haml("commands/name")
     end
 
     on default do
       @commands = JSON.parse(File.read("redis-doc/commands.json"))
+      @title = "Command reference"
 
-      res.write render("views/commands.haml")
+      res.write haml("commands")
     end
   end
 end
