0f8370c2d2c81685523761877a4b684d625c02f1
Michel Martens <michel@soveran.com>
2010-12-21 14:21:03 -0300

    Remove Rack::OpenID.

--- app.rb
+++ app.rb
@@ -86,7 +86,6 @@ end
 Ohm.redis = redis
 
 Cuba.use Rack::Session::Cookie
-Cuba.use Rack::OpenID
 Cuba.use Rack::Static, urls: ["/images"], root: File.join(ROOT_PATH, "public")
 Cuba.use Pistol, Dir[documentation_path + "/**/*.md"] do
   Thread.current[:_cache] && Thread.current[:_cache].clear
