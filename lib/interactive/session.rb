2c5f53a7245e136ef24a263a7670bc23632cfaa9
antirez <antirez@gmail.com>
2014-02-28 10:43:30 +0100

    Interactive CLI fixed to support escapes.
    shellsplit was replaced with cli_split that is able to understand
    escaped literal strings. The code is still not perfect but does a lot
    more what the user expects. For example after the change BITPOS examples
    work producing the right output.

--- lib/interactive/session.rb
+++ lib/interactive/session.rb
@@ -2,6 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + "/redis")
 
 module Interactive
 
+  UNESCAPES = {
+    'a' => "\x07", 'b' => "\x08", 't' => "\x09",
+    'n' => "\x0a", 'v' => "\x0b", 'f' => "\x0c",
+    'r' => "\x0d", 'e' => "\x1b", "\\\\" => "\x5c",
+    "\"" => "\x22", "'" => "\x27"
+  }
+
   # Create and find actions have race conditions, but I don't
   # really care about them right now...
   class Session
@@ -76,27 +83,82 @@ module Interactive
       redis.incr("session:#{namespace}:commands")
     end
 
-    # This method is extracted from the shellwords library. It's
-    # replicated here because the gsub for double quoted and escaped
-    # text had to be removed.
-    def shellsplit(line)
-      words = []
-      field = ''
-      line.scan(/\G\s*(?>([^\s\\\'\"]+)|'([^\']*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|\z)?/) do
-        |word, sq, dq, esc, garbage, sep|
-        raise ArgumentError, "Unmatched double quote: #{line.inspect}" if garbage
-        field << (word || sq || (dq || esc))
-        if sep
-          words << field
-          field = ''
+    def unescape_literal(str)
+      # Escape all the things
+      str.gsub(/\\(?:([#{UNESCAPES.keys.join}])|u([\da-fA-F]{4}))|\\0?x([\da-fA-F]{2})/) {
+        if $1
+          if $1 == '\\' then '\\' else UNESCAPES[$1] end
+        elsif $2 # escape \u0000 unicode
+          ["#$2".hex].pack('U*')
+        elsif $3 # escape \0xff or \xff
+          [$3].pack('H2')
         end
+      }
+    end
+
+    # Parse a command line style list of arguments that can be optionally
+    # delimited by '' or "" quotes, and return it as an array of arguments.
+    #
+    # Strings delimited by "" are unescaped by converting escape characters
+    # such as \n \x.. to their value according to the unescape_literal()
+    # function.
+    #
+    # Example of line that this function can parse:
+    #
+    # "Hello World\n" other arguments 'this is a single argument'
+    #
+    # The above example will return an array of four strings.
+    def cli_split(line)
+      argv = []
+      arg = ""
+      inquotes = false
+      pos = 0
+      while pos < line.length
+        char = line[pos..pos] # Current character
+        isspace = char =~ /\s/
+
+        # Skip empty spaces if we are between strings
+        if !inquotes && isspace
+          if arg.length != 0
+            argv << arg
+            arg = ""
+          end
+          pos += 1
+          next
+        end
+
+         # Append current char to string
+         arg << char
+         pos += 1
+
+         if arg.length == 1 && (char == '"' || char == '\'')
+           inquotes = char
+         elsif arg.length > 1 && inquotes && char == inquotes
+           inquotes = false
+         end
       end
-      words
+      # Put the last argument into the array
+      argv << arg if arg.length != 0
+
+      # We need to make some post-processing.
+      # For strings delimited by '' we just strip initial and final '.
+      # For strings delimited by "" we call unescape_literal().
+      # This is not perfect but should be enough for redis.io interactive
+      # editing.
+      argv.map {|x|
+        if x[0..0] == '"'
+          unescape_literal(x[1..-2])
+        elsif x[0..0] == '\''
+          x[1..-2]
+        else
+          x
+        end
+      }
     end
 
     def _run(line)
       begin
-        arguments = shellsplit(line)
+        arguments = cli_split(line)
       rescue => error
         raise error.message.split(":").first
       end
